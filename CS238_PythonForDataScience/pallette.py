f9f304b0c6743a6e92667decca3371954f013ff3
Gabriel Mathews <mathews.gabriel.45@gmail.com>
2026-02-10 14:56:00 -0800

    Add files via upload

---
+++ CS238_PythonForDataScience/pallette.py
@@ -0,0 +1,31 @@
+pallette = {
+    'black': "#0a0a0a",
+    
+    'dark_blue': '#050544',
+    'blue': '#0a0a8a',
+    'light_blue': '#4040cc',
+    
+    'dark_green': '#054405',
+    'green': '#0a8a0a',
+    'light_green': '#40cc40',
+    
+    'dark_red': '#440505',
+    'red': '#8a0a0a',
+    'light_red': '#cc4040',
+    
+    'dark_purple': '#440544',
+    'purple': '#8a0a8a',
+    'pink': '#cc40cc',
+    
+    'gold': '#444405',
+    'yellow': '#8a8a0a',
+    'light_yellow': '#cccc40',
+    
+    'brown': '#442a05',
+    'orange': '#8a4a0a',
+    'light_orange': '#cc7a3a',
+    
+    'gray': "#8a8a8a",
+    'white': "#fafafa",
+    
+}
